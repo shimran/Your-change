ba09759eaa51a8d571117a7099f8af73f118c2e2
Guest User <student@epicodus.com>
2015-02-11 12:04:23 -0800

    Write initial specs, test out successfully with JavaScript scripts

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+var coinCombinations = (function(amount) {
+   var quarters = parseInt(amount / 25)
+   var remainder = (amount % 25);
+   var dimes = parseInt(remainder / 10)
+   var remainder2 = (remainder % 10);
+   var nickels = parseInt(remainder2 / 5)
+   var remainder3 = (remainder2 % 5);
+   var pennies = parseInt(remainder3 / 1)
+     var dime_amount = dimes.toString().concat(" dime(s) ");
+   var quarter_amount = quarters.toString().concat(" quarter(s) ");
+   var dime_amount = dimes.toString().concat(" dime(s) ");
+   var nickel_amount = nickels.toString().concat(" nickel(s) ");
+   var penny_amount = pennies.toString().concat(" pennie(s)");
+
+
+    if (amount % 25 ==  0) {
+      return quarter_amount
+
+    } else if (amount % 25 != 0 && amount % 10 ==0 ) {
+      return quarter_amount.concat(dime_amount)
+
+    } else if (amount % 10 != 0 && amount % 5 ==0) {
+      return quarter_amount.concat(dime_amount.concat(nickel_amount))
+
+    } else {
+      return quarter_amount.concat(dime_amount.concat(nickel_amount).concat(penny_amount))
+    }
+
+
+});
---
+++ spec/specs.js
@@ -0,0 +1,25 @@
+describe('coinCombinations', function() {
+  it('takes an input of 25 and returns "1 quarter"', function() {
+    expect(coinCombinations(25)).to.equal("1 quarter(s) ");
+  });
+
+  it("takes an input of 50 and returns 2 quarters", function() {
+    expect(coinCombinations(50)).to.equal("2 quarter(s) ");
+  });
+
+  it("takes an input of 60 and returns 2 quarter(s), 1 dime(s)", function() {
+    expect(coinCombinations(60)).to.equal("2 quarter(s) 1 dime(s) ");
+  });
+
+  it("takes an input of 65 and returns 2 quarter(s), 1 dime(s), 1 nickel(s)", function() {
+    expect(coinCombinations(65)).to.equal("2 quarter(s) 1 dime(s) 1 nickel(s) ");
+  });
+
+  it("takes an input of 68 and returns 2 quarter(s), 1 dime(s), 1 nickel(s), 3 pennies", function() {
+    expect(coinCombinations(68)).to.equal("2 quarter(s) 1 dime(s) 1 nickel(s) 3 pennie(s)");
+  });
+
+  it("takes 1 penny and returns 0 quarter(s) 0 dime(s) 0 nickel(s) 1 pennie(s)", function() {
+    expect(coinCombinations(1)).to.equal("0 quarter(s) 0 dime(s) 0 nickel(s) 1 pennie(s)")
+  });
+});
